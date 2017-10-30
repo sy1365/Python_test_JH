feb7ead0929b775e2cd1c4aa81e07a12b6e6875b
Yazdani <SYazdani@jensenhughes.com>
2017-10-30 17:24:38 -0400

    Initial Commit

---
+++ test_mathlib.py
@@ -0,0 +1,9 @@
+import mathlib
+
+def test_calt_total():
+	total = mathlib.calc_total(4,5)
+	assert total == 9
+
+def test_calt_multiply():
+	result = mathlib.calc_multiply(10,3)
+	assert result == 30
